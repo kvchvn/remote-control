2d0a298d5d72290b9eb3a85f4b352c723e43bcc8
Anton Kachan <aa.kvchvn@gmail.com>
2023-01-19 15:11:38 +0300

    feat: create simple websocket server

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+import {WebSocketServer} from "ws";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (data) => {
+        console.log('Received: ', data.toString());
+    });
+    ws.send('Hello World!')
+});
