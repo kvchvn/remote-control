cb3f01bd25c29cccaa51100931791ac285a525b2
Anton Kachan <aa.kvchvn@gmail.com>
2023-01-21 13:29:37 +0300

    feat: implement ability of drawing a circle

--- src/handlers/drawing.ts
+++ src/handlers/drawing.ts
@@ -3,11 +3,30 @@ import { ERRORS } from '../constants.js';
 import { SubCommand } from '../types.js';
 
 const drawRectangle = async ([width, height]: number[]) => {
-  const initialPosition = await mouse.getPosition();
-  await mouse.drag(straightTo(new Point(initialPosition.x, initialPosition.y + height)));
-  await mouse.drag(straightTo(new Point(initialPosition.x + width, initialPosition.y + height)));
-  await mouse.drag(straightTo(new Point(initialPosition.x + width, initialPosition.y)));
-  await mouse.drag(straightTo(new Point(initialPosition.x, initialPosition.y)));
+  const mousePosition = await mouse.getPosition();
+  await mouse.drag(straightTo(new Point(mousePosition.x, mousePosition.y + height)));
+  await mouse.drag(straightTo(new Point(mousePosition.x + width, mousePosition.y + height)));
+  await mouse.drag(straightTo(new Point(mousePosition.x + width, mousePosition.y)));
+  await mouse.drag(straightTo(new Point(mousePosition.x, mousePosition.y)));
+};
+
+const drawCircle = async ([radius]: number[]) => {
+  const mousePosition = await mouse.getPosition();
+  const circleCenter = new Point(mousePosition.x - radius, mousePosition.y);
+
+  let angle = 0;
+  const pointsArray = [];
+  const STEP = 0.01;
+  while (angle <= 2 * Math.PI + STEP) {
+    const point = new Point(
+      circleCenter.x + radius * Math.cos(angle),
+      circleCenter.y + radius * Math.sin(angle),
+    );
+    pointsArray.push(point);
+
+    angle += STEP;
+  }
+  await mouse.drag(pointsArray);
 };
 
 const handleDrawingCommands = async (subCommands: SubCommand, params: number[]) => {
@@ -20,6 +39,9 @@ const handleDrawingCommands = async (subCommands: SubCommand, params: number[]) 
         return;
       case 'square':
         await drawRectangle([params[0], params[0]]);
+        return;
+      case 'circle':
+        await drawCircle(params);
         return;
     }
   }
