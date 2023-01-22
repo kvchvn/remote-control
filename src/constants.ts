c6eda657f7fe5f30d5ca5cfbd8e835c5ff5ce66f
Anton Kachan <aa.kvchvn@gmail.com>
2023-01-22 03:12:58 +0300

    feat: add default width and height of the capture to constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,7 @@ export const BASE_URL = `ws://localhost:${WS_PORT}`;
 export const ERRORS = {
   noParams: 'Params is expected to be passed.',
 };
+
+export const PRINT_SCREEN_WIDTH = 200;
+
+export const PRINT_SCREEN_HEIGHT = 200;
